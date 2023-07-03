1e66890d3362220e520e04faa90614c010d7bcc3
Jordan Kay <jordantkay21@gmail.com>
2023-07-02 22:55:27 -0400

    Bone Collection Logic Phase 2 complete

--- Assets/PROJECT/Script/Managers/SpawnManager.cs
+++ Assets/PROJECT/Script/Managers/SpawnManager.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    [Tooltip("List of all bones within the scene that is available to be spawned")]
+    [SerializeField]
+    private List<GameObject> _bonesToSpawn = new List<GameObject>();
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnBones(5);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void SpawnBones(int boneAmount)
     {
-        
+        for (int i=0; i<boneAmount; i++)
+        {
+            int randomBone = Random.Range(0, _bonesToSpawn.Count - 1);
+            _bonesToSpawn[randomBone].SetActive(true);
+            _bonesToSpawn.RemoveAt(randomBone);
+        }
     }
 }
